230b604cf51bfeff1751f614d96825efad7d8a46
Jaime Valencia <jaimevalencia.yepes@hotmail.com>
2022-01-25 19:08:14 -0500

    Conexiòn y prueba

---
+++ PHP/Conexion.php
@@ -0,0 +1,16 @@
+
+<?php
+$user = "postgres";
+$pass = "root";
+$db = "decameron";
+
+$server = "127.0.0.1";
+$port = "5432";
+
+try {
+    $con = new PDO("pgsql:host=$server;port=$port;dbname=$db", $user, $pass);
+    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Se logro conectar";
+} catch (Exception $e) {
+    echo "No se puedo conectar: " . $e->getMessage();
+}
---
+++ index.php
@@ -0,0 +1,2 @@
+<?php
+header('Location: /PHP/Conexion.php');
